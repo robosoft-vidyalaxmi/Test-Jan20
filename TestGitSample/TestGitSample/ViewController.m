5b4b7ae59d10d8c91591f404f4e65542252163f5
vidyalaxmi <vidz_shenoy@hotmail.com>
2014-01-20 16:20:32 +0530

    corrected braces

--- TestGitSample/TestGitSample/ViewController.m
+++ TestGitSample/TestGitSample/ViewController.m
@@ -26,7 +26,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)buttonClick:(id)sender {
+- (IBAction)buttonClick:(id)sender
+{
     NSLog(@"Click button tapped");
 }
 @end
